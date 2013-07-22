98d2f7ce1a9f1181db49e0cda414b54a7ff59525
Guillaume <guillaume.gregoire.89@gmail.com>
2013-07-22 15:42:26 +0200

    Create example.js

---
+++ example.js
@@ -0,0 +1,20 @@
+var user = { firstname: 'Clark', lastname: 'Kent', job: 'Superman' };
+  	
+log(user);
+		
+log('User added in the db', user);
+		
+log('User added in the db', user, 'yellow');
+log('User added in the db', user, 'orange');
+log('User added in the db', user, 'red');
+log('User added in the db', user, 'magenta');
+log('User added in the db', user, 'violet');
+log('User added in the db', user, 'blue');
+log('User added in the db', user, 'cyan');
+log('User added in the db', user, 'green');
+		
+var logColor = 'yellow';
+log('User added in the db', user);
+		
+logColor = 'green';
+log('User added in the db', user);
