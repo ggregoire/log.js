0d62787066ef45f6c6585786c1776795aac55e29
Guillaume <guillaume.gregoire.89@gmail.com>
2013-07-23 11:49:07 +0200

    Update example.js

--- example.js
+++ example.js
@@ -1,20 +1,23 @@
 var user = { firstname: 'Clark', lastname: 'Kent', job: 'Superman' };
   	
 log(user);
-		
-log('User added in the db', user);
-		
-log('User added in the db', user, 'yellow');
-log('User added in the db', user, 'orange');
-log('User added in the db', user, 'red');
-log('User added in the db', user, 'magenta');
-log('User added in the db', user, 'violet');
-log('User added in the db', user, 'blue');
-log('User added in the db', user, 'cyan');
-log('User added in the db', user, 'green');
-		
-var logColor = 'yellow';
-log('User added in the db', user);
-		
-logColor = 'green';
-log('User added in the db', user);
+
+log('new user', user);
+log('firstname', user.firstname);
+log('firstname length', user.firstname.length);
+log('test the length > 3', user.firstname.length > 3);
+
+log('new user', user, 'yellow');
+log('new user', user, 'orange');
+log('new user', user, 'red');
+log('new user', user, 'magenta');
+log('new user', user, 'violet');
+log('new user', user, 'blue');
+log('new user', user, 'cyan');
+log('new user', user, 'green');
+
+log_settings.color = 'yellow';
+log('new user', user);
+
+log_settings = { background: 'dark', color: 'yellow' };
+log('new user', user);
