df1ba2d4099d8db6f72e44ba82913b1c4bed8c94
Guillaume <guillaume.gregoire.89@gmail.com>
2013-07-23 10:45:51 +0200

    New style

--- log.js
+++ log.js
@@ -1,5 +1,5 @@
 function log() {
   var colors = {'yellow': 'b58900','orange': 'cb4b16','red': 'dc322f','magenta': 'd33682','violet': '6c71c4','blue': '268bd2','cyan': '2aa198','green': '859900'};
-  var color = arguments[2] || (typeof logColor === 'undefined' ? null : logColor) || 'blue';
-  return !arguments[1] ? console.log(arguments[0]) : console.log('%c ' + arguments[0] + ' ', 'background: #eee8d5; color: #' + colors[color] + '; border: 1px solid #' + colors[color] + ';', arguments[1]);
+	var color = arguments[2] || (typeof logColor === 'undefined' ? null : logColor) || 'cyan';
+	return !arguments[1] ? console.log(arguments[0]) : console.log('%c ' + arguments[0] + ' ', 'background:#' + colors[color] + ';color:#fff;font-family:Verdana;padding:1px 4px 2px 4px;border-radius:5px', arguments[1]);
 }
